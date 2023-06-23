b1332add448fa9a40a78ce5331414c67827104b8
hongkanpoon <willphk@outlook.com>
2023-06-22 23:57:48 -0400

    Update Index

--- Passion_Project/Controllers/HomeController.cs
+++ Passion_Project/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Passion_Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +10,25 @@ namespace Passion_Project.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private static readonly HttpClient Client;
+
+        static HomeController()
         {
-            return View();
+            Client = new HttpClient();
+            Client.BaseAddress = new Uri("https://localhost:44338/api/");
         }
 
+        // GET: /
+        public ActionResult Index()
+        {
+            string url = "animedata/listanimes";
+            HttpResponseMessage Response = Client.GetAsync(url).Result;
+            IEnumerable<AnimeDto> Animes = Response.Content.ReadAsAsync<IEnumerable<AnimeDto>>().Result;
+
+            return View(Animes);
+        }
+
+        // GET: /About
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +36,7 @@ namespace Passion_Project.Controllers
             return View();
         }
 
+        // GET: /Contact
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
